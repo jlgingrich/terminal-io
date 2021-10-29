66cf6d2ca3a118f63adb8d1d58a698cb888d9521
Liam's Laptop <66572834+jlgingrich@users.noreply.github.com>
2021-10-28 23:17:24 -0400

    Added Javadocs
    Added Javadoc for all accessible elements, as well as updating the README and adding a package description.

--- src/terminal_io/TerminalIO.java
+++ src/terminal_io/TerminalIO.java
@@ -1,69 +1,80 @@
 package terminal_io;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
 
 /**
+ * A singleton class that automates many basic terminal interactions.
  *
  * @author jlgingrich
  */
 public final class TerminalIO {
 
     /**
-     * Singleton IO class for creating refresh-able "screens" in the
-     * command-line and easily asking a user for input via the command-line,
-     * written by John L. Gingrich
+     * The Scanner used by TerminalIO to get user inputs.
      */
-    public static final Scanner USER = new Scanner(System.in); // Scanner for keyboard inputs, can be publicly read but not redefined
-    private static final List<String> CONTENTS = new ArrayList<>(); // Stores the list of lines in the buffer
-    
-    private static int width = 32; // The width of the printed screen
+    public static final Scanner USER = new Scanner(System.in);
+
+    private static final StringBuilder CONTENTS = new StringBuilder();
+    private static final char WHITESPACE_CHAR = ' ';
+
+    private static int width = 32;
     private static String leftBorder = "";
     private static String rightBorder = "";
 
+    /**
+     * Private constructor defines this as a singleton class.
+     */
     private TerminalIO() {
-        /**
-         * Private, empty constructor defines this as a singleton
-         */
     }
 
+    /**
+     * Writes the given object to the buffer, then renders the buffer.
+     *
+     * @param <T> The type of the object to append.
+     * @param obj The object to append.
+     */
     public static <T> void print(T obj) {
-        /**
-         * Writes the object to the buffer, and renders the buffer all in one.
-         */
         appendLine(obj);
-        renderScreen();
+        renderDisplay();
     }
 
-    public static void screenWidth(int w) {
+    /**
+     * Sets the width if the given width is valid and returns the current width
+     * of the output display
+     *
+     * @param w The new width of the terminal display.
+     */
+    public static void setWidth(int w) {
         /**
-         * Sets the width if the given width is valid and returns the current
-         * width of the output screen
          */
         if (width > 0) {
             width = w;
         }
     }
 
+    /**
+     * Sets the terminal display borders to the provided string.
+     *
+     * @param border The string to be used as the left and right borders.
+     */
     public static void setBorders(String border) {
-        /**
-         * Sets the screen borders to the provided non-empty string.
-         */
         if (!border.isEmpty()) {
             leftBorder = border;
             rightBorder = border;
         }
     }
 
+    /**
+     * Sets the terminal display borders to the provided strings.
+     *
+     * @param left The string to be used as the left border.
+     * @param right The string to be used as the right border.
+     */
     public static void setBorders(String left, String right) {
-        /**
-         * Sets the screen borders to the provided non-empty strings.
-         */
         if (!left.isEmpty()) {
             leftBorder = left;
         }
@@ -72,83 +83,84 @@ public final class TerminalIO {
         }
     }
 
-    public static <T> void appendToLastLine(T obj) {
-        /**
-         * Appends the given object to the last line of the buffer
-         */
-        String currentLine = CONTENTS.get(CONTENTS.size() - 1);
-        CONTENTS.set(CONTENTS.size() - 1, currentLine + obj.toString());
+    /**
+     * Appends the object as a string to the current contents.
+     *
+     * @param <T> The type of the object to append.
+     * @param obj The object to append.
+     */
+    public static <T> void append(T obj) {
+        CONTENTS.append(obj.toString());
     }
 
+    /**
+     * Appends the object as a string to the current contents, then appends a
+     * newline character.
+     *
+     * @param <T> The type of the object to append.
+     * @param obj The object to append.
+     */
     public static <T> void appendLine(T obj) {
-        /**
-         * Appends the given object to the buffer as a new line
-         */
-        CONTENTS.add(obj.toString());
+        CONTENTS.append(obj.toString()).append("\n");
     }
 
-    public static void renderScreen() {
-        /**
-         * Clears the terminal, then writes the buffer into the terminal,
-         * keeping the buffer available in case a refresh is needed. The line is
-         * automatically terminated before the next word that would make the
-         * line longer than the width or, if the word is longer than the width,
-         * before the first character longer than the width.
-         */
-        clearScreen();
-        List<String> printArr = new ArrayList<>(CONTENTS); // Stores the list of lines in the buffer
-        for (int i = 0; i < printArr.size(); i++) {
-            if (printArr.get(i).length() > width) {
-                String[] s;
-                s = printArr.get(i).split(" ");
-                if (s[0].length() > width) {
-                    String[] sCopy = new String[s.length + 1];
-                    sCopy[0] = s[0].substring(0, width);
-                    sCopy[1] = s[0].substring(width);
-                    System.arraycopy(s, 1, sCopy, 2, s.length - 1);
-                    s = sCopy;
+    /**
+     * Appends a newline character to the current contents.
+     */
+    public static void appendNewLine() {
+        CONTENTS.append("\n");
+    }
+
+    /**
+     * Attempt to flush the terminal display, then writes the current contents
+     * to the terminal display, wrapping the characters as necessary and
+     * appending the border strings to the start and end of each line.
+     */
+    public static void renderDisplay() {
+        clearDisplay();
+        StringBuilder currentLine = new StringBuilder();
+        for (int i = 0; i < CONTENTS.length(); i++) {
+            String c = String.valueOf(CONTENTS.charAt(i));
+            if (c.equals("\n")) { // If it reads a newline character, print the completed line and reset
+                while (currentLine.length() < width) {
+                    currentLine.append(WHITESPACE_CHAR);
                 }
-                StringBuilder newLine = new StringBuilder();
-                StringBuilder nextLine = new StringBuilder();
-                int j = 0;
-                while (newLine.length() + s[j].length() <= width) {
-                    newLine.append(s[j]);
-                    if (newLine.length() + s[j + 1].length() <= width) {
-                        newLine.append(" ");
+                System.out.println(leftBorder + " " + currentLine + " " + rightBorder);
+                currentLine.delete(0, currentLine.length());
+            } else { // if it reads a normal character
+                if (currentLine.length() + 1 > width) { // If the next character won't fit on the same line
+                    while (currentLine.length() < width) {
+                        currentLine.append(WHITESPACE_CHAR);
                     }
-                    j++;
+                    System.out.println(leftBorder + " " + currentLine + " " + rightBorder);
+                    currentLine.delete(0, currentLine.length());
                 }
-                while (j < s.length) {
-                    nextLine.append(s[j]);
-                    if (j + 1 < s.length) {
-                        nextLine.append(" ");
-                    }
-                    j++;
-                }
-                printArr.set(i, newLine.toString());
-                printArr.add(i + 1, nextLine.toString());
+                currentLine.append(c);
             }
-            StringBuilder line = new StringBuilder(printArr.get(i));
-            while (line.length() < width) {
-                line.append(" ");
+        }
+        if (!currentLine.isEmpty()) {
+            while (currentLine.length() < width) {
+                currentLine.append(WHITESPACE_CHAR);
             }
-            System.out.println(leftBorder + " " + line.toString() + " " + rightBorder);
+            System.out.println(leftBorder + " " + currentLine + " " + rightBorder);
         }
     }
 
+    /**
+     * Clears the current contents.
+     */
     public static void clear() {
-        /**
-         * Clears the buffer.
-         */
-        CONTENTS.clear();
+        CONTENTS.delete(0, CONTENTS.length());
     }
 
-    public static void clearScreen() {
-        /**
-         * Tries to clear the terminal, printing an error if the process fails
-         *
-         * From https://www.delftstack.com/howto/java/java-clear-console/
-         */
+    /**
+     * Attempts to flush the terminal display. Should work for most operating
+     * systems, but if it fails, it will print out the resulting exception.
+     *
+     * @see
+     * <a href="https://www.delftstack.com/howto/java/java-clear-console/">Source</a>
+     */
+    public static void clearDisplay() {
         try {
             String operatingSystem = System.getProperty("os.name"); //Check the current operating system
 
@@ -159,97 +171,179 @@ public final class TerminalIO {
             } else {
                 ProcessBuilder pb = new ProcessBuilder("clear");
                 Process startProcess = pb.inheritIO().start();
-
                 startProcess.waitFor();
             }
         } catch (IOException | InterruptedException e) {
-            print(e);
+            System.out.print(e);
         }
     }
 
-    public static String getResponse(String msg, String targetCommand) {
-        /**
-         * Appends the given message to the buffer, then waits for the user to
-         * successfully enter the desired string, asking again if the String
-         * provided is invalid, after which it clears the buffer and returns the
-         * provided command.
-         */
+    /**
+     * Appends the given prompt to the current contents, then refreshes the
+     * terminal display until the user enters the desired string.
+     *
+     * @param msg The message displayed to the user.
+     * @param desiredCommand The valid response string.
+     * @return A string containing the user's last input, which should always be
+     * equal to the valid response string.
+     */
+    public static String getResponse(String msg, String desiredCommand) {
         String command;
         TerminalIO.appendLine(msg);
         do {
-            TerminalIO.renderScreen();
+            TerminalIO.renderDisplay();
             command = USER.nextLine().trim();
-        } while (!targetCommand.equals(command)); // Try again if the trimmed command isn't the wanted command
+        } while (!desiredCommand.equals(command));
         TerminalIO.clear();
         return command;
     }
 
+    /**
+     * Appends the given prompt to the current contents, then refreshes the
+     * terminal display until the user enters a string contained in the given
+     * string array.
+     *
+     * @param msg The message displayed to the user.
+     * @param validCommands An array of all valid response strings.
+     * @return A string containing the user's last input.
+     */
     public static String getResponse(String msg, String[] validCommands) {
-        /**
-         * Appends the given message to the buffer, then waits for the user to
-         * successfully enter a String that is contained in the given array,
-         * asking again if the String provided is invalid, after which it clears
-         * the buffer and returns the provided command.
-         */
         String command;
         TerminalIO.appendLine(msg);
         do {
-            TerminalIO.renderScreen();
+            TerminalIO.renderDisplay();
             command = USER.nextLine().trim();
         } while (!Arrays.asList(validCommands).contains(command));
         TerminalIO.clear();
         return command;
     }
 
+    /**
+     * Appends the given prompt to the current contents, then refreshes the
+     * terminal display until the user enters a string contained in the given
+     * collection.
+     *
+     * @param msg The message displayed to the user.
+     * @param validCommands A collection of all valid response strings.
+     * @return A string containing the user's last input.
+     */
     public static String getResponse(String msg, Collection<String> validCommands) {
-        /**
-         * Appends the given message to the buffer, then waits for the user to
-         * successfully enter a String that is contained in the given collection
-         * of Strings, asking again if the String provided is invalid, after
-         * which it clears the buffer and returns the provided command.
-         */
         String command;
         TerminalIO.appendLine(msg);
         do {
-            TerminalIO.renderScreen();
+            TerminalIO.renderDisplay();
             command = USER.nextLine().trim();
         } while (!validCommands.contains(command));
         TerminalIO.clear();
         return command;
     }
 
+    /**
+     * Appends the given prompt to the current contents, then refreshes the
+     * terminal display until the user enters a string for which the validator
+     * returns true.
+     *
+     * @param msg The message displayed to the user.
+     * @param validator A function that tests user inputs to determine a valid
+     * response string.
+     * @return A string containing the user's last input.
+     */
     public static String getResponse(String msg, Function<String, Boolean> validator) {
-        /**
-         * Appends the given message to the buffer, then waits for the user to
-         * successfully enter a String for which the validator function returns
-         * true, asking again if the String provided is invalid, after which it
-         * clears the buffer and returns the provided command.
-         */
         String command;
         TerminalIO.appendLine(msg);
         do {
-            TerminalIO.renderScreen();
+            TerminalIO.renderDisplay();
             command = USER.nextLine().trim();
         } while (!validator.apply(command));
         TerminalIO.clear();
         return command;
     }
 
+    /**
+     * Instructs the user to press the 'enter' key, then refreshes the terminal
+     * display until the user enters an empty string.
+     */
     public static void getContinue() {
-        /**
-         * Asks the user to press 'Enter' to continue, asking again if user
-         * types anything else, after which it clears the buffer and resumes
-         * execution.
-         */
-        getResponse("Press 'enter' to continue", ""); // No sense returning an empty string
+        getResponse("Press 'enter' to continue", "");
     }
-    
+
+    /**
+     * Presents the user with the given prompt, then refreshes the terminal
+     * display until the user enters an empty string.
+     *
+     * @param msg The message displayed to the user.
+     */
     public static void getContinue(String msg) {
-        /**
-         * Asks the user to press 'Enter' to continue with a custom prompt,
-         * asking again if user types anything else, after which it clears the
-         * buffer and resumes execution.
-         */
-        getResponse(msg, ""); // No sense returning an empty string
+        getResponse(msg, "");
     }
+
+    /**
+     * A single-argument function that tests if the input can be cleanly
+     * converted to an integer. Can be used as a validator.
+     *
+     * @see #getResponse(java.lang.String, java.util.function.Function)
+     */
+    public static final Function<String, Boolean> tryStringToInteger = s -> {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    };
+
+    /**
+     * A single-argument function that tests if the input can be cleanly
+     * converted to a double. Can be used as a validator.
+     *
+     * @see #getResponse(java.lang.String, java.util.function.Function)
+     */
+    public static final Function<String, Boolean> tryStringToDouble = s -> {
+        try {
+            Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    };
+
+    /**
+     * A single-argument function that tests if the input can be cleanly
+     * converted to a long. Can be used as a validator.
+     *
+     * @see #getResponse(java.lang.String, java.util.function.Function)
+     */
+    public static final Function<String, Boolean> tryStringToLong = s -> {
+        try {
+            Long.parseLong(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    };
+
+    /**
+     * A single-argument function that tests if the input can be cleanly
+     * converted to a byte. Can be used as a validator.
+     *
+     * @see #getResponse(java.lang.String, java.util.function.Function)
+     */
+    public static final Function<String, Boolean> tryStringToByte = s -> {
+        try {
+            Byte.parseByte(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    };
+
+    /**
+     * A single-argument function that tests if the input can be cleanly
+     * converted to a char. Can be used as a validator.
+     *
+     * @see #getResponse(java.lang.String, java.util.function.Function)
+     */
+    public static final Function<String, Boolean> tryStringToCharacter = s -> {
+        return s.length() == 1;
+    };
 }
---
+++ src/terminal_io/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A package automating many basic terminal interactions that allows students
+ * learning Java to focus on backend code rather than boilerplate display code
+ * while still allowing for flexible customization.
+ * @author jlgingrich
+ */
+package terminal_io;
